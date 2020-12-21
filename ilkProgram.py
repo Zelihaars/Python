04f177098d58bdede095e32e93ed5d85968071bd
Zelihaars <72877610+Zelihaars@users.noreply.github.com>
2020-12-21 21:10:06 +0300

    Add files via upload

---
+++ ilkProgram.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("2+2")
+maasX=5000
+vergi=0.27
+print(maasX-(maasX*vergi))
+number1=10
+print(number1)
+number1+=30 #Artırma 
+print(number1)
+x=10 #Tek satırda da atama yapılabilir.
+y=20 # x , y = (10 , 20 )
+x1="10"
+y1="20"
+print(x+y)
+print(x1+y1)#Değerler "" içinde yani string türünde olduğu için toplama yapılmaz.
+a= input('1.sayıyı girin : ' )#Bunları string değerden integer değerine dönüştürmemiz lazım .
+b= input('2.sayıyı girin : ' )#Başlarına int koyarsak işlem gerçekleşir.
+toplam=a+b
+print(toplam)
+a1= input('1.sayıyı girin : ' )
+b1= input('2.sayıyı girin : ' )
+toplam1=int(a1)+int(b1)
+print(toplam1)
