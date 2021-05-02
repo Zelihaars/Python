cc3381f39041ba96070774c7aab20cc06ac5515e
Zelihaars <72877610+Zelihaars@users.noreply.github.com>
2021-05-02 17:42:40 +0300

    Update Odev1.py

--- GlobalAlhubHomework/Odev1.py
+++ GlobalAlhubHomework/Odev1.py
@@ -1,1 +1,14 @@
-
+print("5 adet değer girin ")
+deger1=input("1.yi girin:" )
+deger2=input("2.yi girin:" )
+deger3=input("3.yi girin:" )
+deger4=input("4.yü girin:" )
+deger5=input("5.yi girin:" )
+listem=[deger1,deger2,deger3,deger4,deger5]
+print(listem)
+for i in range(5):
+    sıra =listem[i]
+    tip = type(listem[i])
+    print(f"{i+1}inci deger {sıra} tipi : {tip}")
+print(listem)    
+© 2021 GitHub, Inc.
